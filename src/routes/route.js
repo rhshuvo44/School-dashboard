fb777f2ceaee9cf6d24e073c663d591ac08505c8
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-09-12 17:48:03 +0600

    side bar added

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,25 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import Resetpassword from "../pages/Resetpassword";
-import Forgotpassword from "../pages/Forgotpassword";
+// import Resetpassword from "../pages/Resetpassword";
+// import Forgotpassword from "../pages/Forgotpassword";
 import MainLayout from "../layout/MainLayout";
 import Students from "../pages/Students";
 import Dashboard from "../pages/Dashboard.Jsx";
-import Login from "../pages/Login";
+// import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/reset-password",
-    element: <Resetpassword />,
-  },
-  {
-    path: "/forgot-password",
-    element: <Forgotpassword />,
-  },
+  // {
+  //   path: "/login",
+  //   element: <Login />,
+  // },
+  // {
+  //   path: "/reset-password",
+  //   element: <Resetpassword />,
+  // },
+  // {
+  //   path: "/forgot-password",
+  //   element: <Forgotpassword />,
+  // },
   {
     path: "/",
     element: <MainLayout />,
