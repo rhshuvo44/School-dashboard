aedfd71899eb70d6dc518f1fbec5dc99188f30e5
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-09-18 17:56:26 +0600

    password id update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,60 +32,60 @@ const Login = () => {
         </div>
         <div className="card w-full md:w-1/2 shadow-2xl bg-base-100 max-w-md">
           <div className="card-body">
-          <form onSubmit={handleSubmit(onSubmit)} className="py-5">
-                <div className="form-control w-full max-w-xs mt-5">
-                 <label
-                    htmlFor="email"
-                    className="inline-block w-full text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
-                  >
-                    Email
-                  </label>
-                  <div className="relative flex items-center my-2">
-                    <input
-                      id="email"
-                      type="email"
-                      placeholder="Email Address"
-                      {...register("email", { required: true })}
-                      className="peer relative h-10 w-full rounded-md bg-gray-50 pl-10 pr-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400 focus:drop-shadow-lg"
-                    />
-                    <AiFillMail className="material-symbols-outlined absolute left-2 transition-all duration-200 ease-in-out group-focus-within:text-blue-400" />
-                  </div>
-                </div>
-                <p className="text-xs text-error">{errors.email?.message}</p>
-                <div className="form-control w-full max-w-xs mt-5">
-                  <label
-                    htmlFor="email"
-                    className="inline-block w-full text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
-                  >
-                    Password
-                  </label>
-                  <div className="relative flex items-center my-2">
-                    <input
-                      id="email"
-                      type="password"
-                      placeholder="Password"
-                      {...register("password", { required: true })}
-                      className="peer relative h-10 w-full rounded-md bg-gray-50 pl-10 pr-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400 focus:drop-shadow-lg"
-                    />
-                    <RiLockPasswordLine className="material-symbols-outlined absolute left-2 transition-all duration-200 ease-in-out group-focus-within:text-blue-400" />
-                  </div>
-                </div>
-                <p className="text-xs text-error">{errors.password?.message}</p>
-                <div className="flex flex-col md:flex-row justify-between my-2 gap-5">
-                  <Link className="capitalize underline" to="/">
-                    create new Account
-                  </Link>
-                  <Link className="text-error capitalize" to="forgot-password">
-                    Forgot Password?
-                  </Link>
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-block mt-5 bg-primary capitalize text-white hover:bg-secondary"
+            <form onSubmit={handleSubmit(onSubmit)} className="py-5">
+              <div className="form-control w-full max-w-xs mt-5">
+                <label
+                  htmlFor="email"
+                  className="inline-block w-full text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
                 >
-                  Login
-                </button>
-              </form>
+                  Email
+                </label>
+                <div className="relative flex items-center my-2">
+                  <input
+                    id="email"
+                    type="email"
+                    placeholder="Email Address"
+                    {...register("email", { required: true })}
+                    className="peer relative h-10 w-full rounded-md bg-gray-50 pl-10 pr-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400 focus:drop-shadow-lg"
+                  />
+                  <AiFillMail className="material-symbols-outlined absolute left-2 transition-all duration-200 ease-in-out group-focus-within:text-blue-400" />
+                </div>
+              </div>
+              <p className="text-xs text-error">{errors.email?.message}</p>
+              <div className="form-control w-full max-w-xs mt-5">
+                <label
+                  htmlFor="password"
+                  className="inline-block w-full text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
+                >
+                  Password
+                </label>
+                <div className="relative flex items-center my-2">
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                    {...register("password", { required: true })}
+                    className="peer relative h-10 w-full rounded-md bg-gray-50 pl-10 pr-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400 focus:drop-shadow-lg"
+                  />
+                  <RiLockPasswordLine className="material-symbols-outlined absolute left-2 transition-all duration-200 ease-in-out group-focus-within:text-blue-400" />
+                </div>
+              </div>
+              <p className="text-xs text-error">{errors.password?.message}</p>
+              <div className="flex flex-col md:flex-row justify-between my-2 gap-5">
+                <Link className="capitalize underline" to="/">
+                  create new Account
+                </Link>
+                <Link className="text-error capitalize" to="forgot-password">
+                  Forgot Password?
+                </Link>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-block mt-5 bg-primary capitalize text-white hover:bg-secondary"
+              >
+                Login
+              </button>
+            </form>
           </div>
         </div>
       </div>
